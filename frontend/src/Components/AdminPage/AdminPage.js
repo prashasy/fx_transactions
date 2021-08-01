11f96d28801d70a12408ea6e456098810b7cd56b
prashasy <prashasyashok@gmail.com>
2021-08-01 23:03:35 +0530

    admin page: modal size adjustment

--- frontend/src/Components/AdminPage/AdminPage.js
+++ frontend/src/Components/AdminPage/AdminPage.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './AdminPage.css';
-import axios from 'axios';
-import { useParams } from "react-router-dom";
 import { SegmentedControl } from 'segmented-control'
 import { Transactions } from './Components/Transactions';
 
 const AdminPage = () => {
-    // const [transactionsSummary, setTransactionsSummary] = useState();
     const [selectedSegment, setSelectedSegment] = useState();
-    // const { branch: branchId } = useParams();
-    // useEffect(() => {
-    //     axios.get(`http://localhost:3001/admin/${branchId}`).then((res) => {
-    //         console.log(res.data)
-    //     });
-    // });
     return (
         <div className="page-container">
             <div className="header-container">
-                <span>Admin Dashboard</span>
+                <h2>Admin Dashboard</h2>
             </div>
             <div className="body-container">
                 <div className="segment-controller">
@@ -30,7 +21,7 @@ const AdminPage = () => {
                             { label: "Summary", value: "SUMMARY" }
                         ]}
                         setValue={setSelectedSegment}
-                        style={{ width: '60%', color: '#ab47bc', border: '2px solid', borderRadius: '8px 8px', fontSize: 'small' }} // purple400
+                        style={{ width: '60%', color: 'rgb(8,100,100,1)', border: '2px solid', borderRadius: '8px 8px', fontSize: 'small' }} // purple400
                     />
                 </div>
                 <div className="data-container">
